8323d9dc2333adaa33ec8f2658657fe739c75b36
João <jocolognesi@gmail.com>
2025-08-08 15:31:31 -0300

    ex00

--- ex00/ClapTrap.cpp
+++ ex00/ClapTrap.cpp
@@ -4,10 +4,32 @@
 
 ClapTrap::ClapTrap() 
 	: _name("Default"), _hitPoints(10), _energyPoints(10), _attackDamage(0) {
-	std::cout << "ClapTrap default constructor called" << std::endl;
+	std::cout << "Default constructor called" << std::endl;
 }
 
 ClapTrap::ClapTrap(std::string name)
 	: _name(name), _hitPoints(10), _energyPoints(10), _attackDamage(0) {
-	std::cout << "ClapTrap" << _name << "constructor called" << std::endl;
+	std::cout << "Named constructor called" << std::endl;
 }
+
+ClapTrap::ClapTrap(const ClapTrap& other) {
+	*this = other;
+	std::cout << "Copy constructor called" << std::endl;
+}
+
+ClapTrap::~ClapTrap() {
+	std::cout << "Destructor called" << std::endl;
+}
+
+ClapTrap& ClapTrap::operator=(const ClapTrap& other) {
+	if (this != &other)
+	{
+		this->_name = other._name;
+		this->_hitPoints = other._hitPoints;
+		this->_energyPoints = other._energyPoints;
+		this->_attackDamage = other._attackDamage;		
+	}
+	std::cout << "Copy assignment called" << std::endl;
+	return *this;
+}
+
