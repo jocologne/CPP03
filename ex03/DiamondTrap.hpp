87457c138ce90e1987b5618b0c67b1d1c5c31c20
João <jocolognesi@gmail.com>
2025-08-17 01:42:14 -0300

    ex03

---
+++ ex03/DiamondTrap.hpp
@@ -0,0 +1,20 @@
+#ifndef DIAMONDTRAP_HPP
+#define DIAMONDTRAP_HPP
+
+#include "ScavTrap.hpp"
+#include "FragTrap.hpp"
+
+class DiamondTrap : public ScavTrap, public FragTrap {
+private:
+	std::string _name;
+public:
+	DiamondTrap();
+	explicit DiamondTrap(const std::string& name);
+	DiamondTrap(const DiamondTrap& other);
+	~DiamondTrap();
+	DiamondTrap& operator=(const DiamondTrap& other);
+	void whoAmI() const;
+	using ScavTrap::attack;
+};
+
+#endif
