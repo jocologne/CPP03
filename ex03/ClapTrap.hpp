4c4a530278ca148f870ba5c65ece3703db3fc9fa
João <jocolognesi@gmail.com>
2025-08-17 00:45:13 -0300

    ex03

---
+++ ex03/ClapTrap.hpp
@@ -0,0 +1,32 @@
+
+#ifndef CLAPTRAP_HPP
+#define CLAPTRAP_HPP
+
+#include <string>
+
+class ClapTrap {
+protected:
+	std::string		_name;
+	unsigned int	_hitPoints;
+	unsigned int	_energyPoints;
+	unsigned int	_attackDamage;
+public:
+	//Constructor and Destructor
+	ClapTrap();
+	ClapTrap(std::string name);
+	ClapTrap(const ClapTrap& other);
+	virtual ~ClapTrap();
+	//Operator
+	ClapTrap& operator=(const ClapTrap& other);
+	//Functions
+	void attack(const std::string& target);
+	void takeDamage(unsigned int amount);
+	void beRepaired(unsigned int amount);
+	//Getters
+	std::string getName() const;
+	unsigned int getHitPoints() const;
+	unsigned int getEnergyPoints() const;
+	unsigned int getAttackDamage() const;
+};
+
+#endif
